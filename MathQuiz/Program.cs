7f650c6571e9a7956edbeb63bb5e7f44d92e7eac
NotRainyz <149025003+NotRainyz@users.noreply.github.com>
2023-10-30 17:26:08 -0400

    Quiz Console App!

---
+++ MathQuiz/Program.cs
@@ -0,0 +1,106 @@
+﻿// Console Settings
+Console.Title = "Math Quiz!";
+Console.ForegroundColor = ConsoleColor.Magenta;
+
+// Welcome Message
+Console.WriteLine("Hello! Welcome to the math quiz! Are you ready?");
+
+string? response = Console.ReadLine();
+
+if (response == "yes")
+{
+    Console.WriteLine("Good Luck!");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Out of here bozo!");
+    Environment.Exit(0);
+}
+
+// Math Problems
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+// 1)
+Console.WriteLine("What is, 2 + 2 * 17 + 38?");
+
+string? answer1 = Console.ReadLine();
+
+if (answer1 == "74")
+{
+    Console.WriteLine("Correct! On to the next question.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Out of here bozo!");
+    Environment.Exit(0);
+}
+// 2)
+Console.WriteLine("What is, 32^2 + 17 * 13?");
+
+string? answer2 = Console.ReadLine();
+
+if (answer2 == "1245")
+{
+    Console.WriteLine("Correct! On to the next question.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Out of here bozo!");
+    Environment.Exit(0);
+}
+
+// 3)
+Console.WriteLine("What is, the square root of 1245? Simplify to a whole number if needed.");
+
+string? answer3 = Console.ReadLine();
+
+if (answer3 == "35")
+{
+    Console.WriteLine("Correct! On to the next question.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Out of here bozo!");
+    Environment.Exit(0);
+}
+
+// 4)
+Console.WriteLine("What is, 17^4 + 13 * 9 * 0 + 535?");
+
+string? answer4 = Console.ReadLine();
+
+if (answer4 == "535")
+{
+    Console.WriteLine("Correct! On to the next question.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Out of here bozo!");
+    Environment.Exit(0);
+}
+
+// 5)
+Console.WriteLine("Final Question! What is, 11^2 + 13");
+
+string? answer5 = Console.ReadLine();
+
+if (answer5 == "134")
+{
+    Console.WriteLine("Ding Ding Ding! Winner Winner Chicken Dinner!");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("So Close! Cya Bozo");
+    Environment.Exit(0);
+}
+
+// Congrats Message
+
+// End
+Console.ReadLine();
